54d491e88758751c07ea263801f7299a9a452af0
Philippe Charrière <ph.charriere@gmail.com>
2022-10-31 06:25:30 +0000

    :sparkles: Introduce new features. Add log host function

--- 05-use-host-function-helpers/functions/hey/src/main.rs
+++ 05-use-host-function-helpers/functions/hey/src/main.rs
@@ -1,30 +1,21 @@
 use pre_hf;
 
-
 fn main() {
   // foo
+  pre_hf::log(&"🚀🚀🚀".to_owned());
+
 }
 
 
 fn print_hello(name: &String) {
-  //pre_hf::log(&"👋👋👋 hello world 🌍".to_owned());
-
   pre_hf::log(&["👋 hello ", name].concat());
-  //log(&["👋 hello ", name].concat());
 }
 
-/// WebAssembly export that accepts a string (linear memory offset, byteCount)
-/// and calls [`greet`].
-///
-/// Note: The input parameters were returned by [`allocate`]. This is not an
-/// ownership transfer, so the inputs can be reused after this call.
 #[cfg_attr(all(target_arch = "wasm32"), export_name = "print_hello")]
 #[no_mangle]
 pub unsafe extern "C" fn _print_hello(ptr: u32, len: u32) {
   print_hello(&pre_hf::ptr_to_string(ptr, len));
 }
-
-
 
 #[no_mangle]
 pub fn add(a: i32, b: i32) -> i32 {
